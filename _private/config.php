8e152b1312781850093626dfa0b0bed6901af96e
Petr Blažek <blazek@petbla.cz>
2019-03-21 00:12:31 +0100

    Set gitignore

--- _private/config.php
+++ _private/config.php
@@ -1,7 +1,51 @@
 <?php
 $config = array();
+
+/*
+ *************************************************************************************************
+ *                                  MySQL Database                                               *
+ *************************************************************************************************
+ */
 $config['db_host'] = 'localhost';
 $config['db_user'] = 'root';	
 $config['db_pass'] = 'Heslo01!';
 $config['db_name'] = 'intranet';
+
+/*
+ *************************************************************************************************
+ *                                  Customer Settings  Webpage                                   *
+ *************************************************************************************************
+ */
+$config['sitename']         = 'Intranet - DMS';
+$config['metadescription']  = 'Intranet - správa dokumentů';
+$config['siteshortname']    = 'Intranet';
+$config['headtitle']        = 'Intranet';
+$config['siteurl']          = 'http://petblanb/intranet/';
+
+// OBEC
+$config['fileserver']       = 'http://petblanb/FileServer/';
+$config['webserver']         = 'http://petblanb/intranet/fileserver/';
+$config['dbPrefix']         = 'mis_'; 
+$config['compName']         = 'OBEC Mistřice';
+
+// Testing
+//$config['fileserver']       = 'http://venuse/eBook/MySQL/';                                                                        
+//$config['webserver']        = 'http://venuse:5000/eBook/MySQL/';   
+//$config['dbPrefix']         = 'test_'; 
+//$config['compName']         = 'TEST Zkušební společnost (VENUSE)';
+
+// Zahrádkáři
+//$config['fileserver']       = 'ftp://venuse/users/petr/Job/Zahradkari/';
+//$config['webserver']         = 'http://petblanb/intranet/fileserver/';
+//$config['dbPrefix']         = 'czs_';   
+
+/**
+ *  Address
+ */
+$config['compAddress']   = 'Mistřice 9';
+$config['compCity']      = 'Mistřice';
+$config['compZip']       = '68712';
+$config['compICO']       = '00267267';
+
+
 ?>
--- registry/config.php
+++ registry/config.php
@@ -11,46 +11,6 @@
 require_once('_private/config.php');
 
 
-/*
- *************************************************************************************************
- *                                  Customer Settings  Webpage                                   *
- *************************************************************************************************
- */
-$config['sitename']         = 'Intranet - DMS';
-$config['metadescription']  = 'Intranet - správa dokumentů';
-$config['siteshortname']    = 'Intranet';
-$config['headtitle']        = 'Intranet';
-$config['siteurl']          = 'http://petblanb/intranet/';
-
-// OBEC
-$config['fileserver']       = 'http://petblanb/FileServer/';
-$config['webserver']         = 'http://petblanb/intranet/fileserver/';
-$config['dbPrefix']         = 'mis_'; 
-$config['compName']         = 'OBEC Mistřice';
-
-// Testing
-//$config['fileserver']       = 'http://venuse/eBook/MySQL/';                                                                        
-//$config['webserver']        = 'http://venuse:5000/eBook/MySQL/';   
-//$config['dbPrefix']         = 'test_'; 
-//$config['compName']         = 'TEST Zkušební společnost (VENUSE)';
-
-// Zahrádkáři
-//$config['fileserver']       = 'ftp://venuse/users/petr/Job/Zahradkari/';
-//$config['webserver']         = 'http://petblanb/intranet/fileserver/';
-//$config['dbPrefix']         = 'czs_';   
-
-/**
- *  Address
- */
-$config['compAddress']   = 'Mistřice 9';
-$config['compCity']      = 'Mistřice';
-$config['compZip']       = '68712';
-$config['compICO']       = '00267267';
-
-/*
-*************************************************************************************************
-*/
-
 /**
  *  Web setting
  */ 
