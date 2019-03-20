49089a3f6d23163fc705ee89a07a7a15f6fc256a
petr <petr@SERVER.MISTRICE.local>
2019-03-21 00:32:48 +0100

    Lokální změna

--- _private/config.php
+++ _private/config.php
@@ -1,43 +1,31 @@
 <?php
-$config = array();
-
 /*
  *************************************************************************************************
  *                                  MySQL Database                                               *
  *************************************************************************************************
  */
+$config = array();
 $config['db_host'] = 'localhost';
 $config['db_user'] = 'root';	
-$config['db_pass'] = 'Heslo01!';
+$config['db_pass'] = 'rumBA#21';
 $config['db_name'] = 'intranet';
-
 /*
- ************************************************************************************************
- *                                  Customer Settings  Webpage                                  *
- ************************************************************************************************
+ *************************************************************************************************
+ *                                  Customer Settings  Webpage                                   *
+ *************************************************************************************************
  */
 $config['sitename']         = 'Intranet - DMS';
 $config['metadescription']  = 'Intranet - správa dokumentů';
 $config['siteshortname']    = 'Intranet';
 $config['headtitle']        = 'Intranet';
-$config['siteurl']          = 'http://petblanb/intranet/';
+$config['siteurl']          = 'http://server/intranet/';
 
 // OBEC
-$config['fileserver']       = 'http://petblanb/FileServer/';
-$config['webserver']         = 'http://petblanb/intranet/fileserver/';
+$config['fileserver']       = 'http://server/FileServer/';
+$config['webserver']        = 'file://server/fileserver/';
 $config['dbPrefix']         = 'mis_'; 
 $config['compName']         = 'OBEC Mistřice';
 
-// Testing
-//$config['fileserver']       = 'http://venuse/eBook/MySQL/';                                                                        
-//$config['webserver']        = 'http://venuse:5000/eBook/MySQL/';   
-//$config['dbPrefix']         = 'test_'; 
-//$config['compName']         = 'TEST Zkušební společnost (VENUSE)';
-
-// Zahrádkáři
-//$config['fileserver']       = 'ftp://venuse/users/petr/Job/Zahradkari/';
-//$config['webserver']         = 'http://petblanb/intranet/fileserver/';
-//$config['dbPrefix']         = 'czs_';   
 
 /**
  *  Address
@@ -47,5 +35,8 @@ $config['compCity']      = 'Mistřice';
 $config['compZip']       = '68712';
 $config['compICO']       = '00267267';
 
+/*
+*************************************************************************************************
+*/
 
 ?>
