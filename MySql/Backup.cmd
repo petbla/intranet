df0df83fed75ecbeea6c52e6d84f8891f0de4986
petr <petr@SERVER.MISTRICE.local>
2019-11-10 20:41:23 +0100

    Update on SERVER

--- MySql/Backup.cmd
+++ MySql/Backup.cmd
@@ -1,5 +1,7 @@
 rem Connect to Database
 rem mysql --defaults-extra-file=.\_private\mysqldump.cnf intranet
+rem mysql -u root -p --password=rumBA#21 intranet
 
 rem Full Backup Databse Intranet
 mysqldump --defaults-extra-file=.\_private\mysqldump.cnf intranet > c:\temp\intranet.sql
+mysqldump -u root -p --password=rumBA#21 intranet --single-transaction > c:\temp\intranet.sql
